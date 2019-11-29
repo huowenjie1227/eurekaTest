277548e0f268744af323d95b8420b8a4527f6efe
huowenjie1227 <2153514946@qq.com>
2019-11-29 19:29:53 +0800

    一次提交

--- eurekaCli02/src/main/java/com/huowenjie/api/Test01Api.java
+++ eurekaCli02/src/main/java/com/huowenjie/api/Test01Api.java
@@ -3,7 +3,7 @@ package com.huowenjie.api;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient(name = "EUREKA-CLIENT01",url = "http://localhost:8083")
+@FeignClient(name = "EUREKA-CLIENT01",url = "http://localhost:8085")
 public interface Test01Api {
 
     @RequestMapping("test01")
